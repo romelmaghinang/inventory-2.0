6ec09a6237685a06bd13966b3f376b56160331ba
candysizzl <theresa@sharpeconceptsny.com>
2024-06-29 03:55:01 +0800

    id retrieve for sales order - for testing

--- app/Http/Controllers/CarrierController.php
+++ app/Http/Controllers/CarrierController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Carrier;
+use Illuminate\Support\Facades\Request;
 
 class CarrierController extends Controller
 {
-    //
+    public function getCarrierId(Request $request)
+    {
+        $carrierId = $request->input('carrierId');
+        $carrier = new Carrier();
+        return $carrier->getCarrierIdByName($carrierId);
+    }
+
+    public function getCarrierServiceId(Request $request)
+    {
+        $carrierServiceId = $request->input('carrierServiceId');
+        $carrier = new Carrier();
+        return $carrier->getCarrierServiceIdByName($carrierServiceId);
+    }
 }
+
