040b3af44b3c51c64228c9caae852f882d2e1361
candysizzl <theresa@sharpeconceptsny.com>
2024-06-28 23:42:16 +0800

    still to be modified - sales order

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -10,31 +10,22 @@ class SalesOrderController extends Controller
 {
     public function create(Request $request)
     {
+        // Validate the request data
         $validator = Validator::make($request->all(), [
             'status' => 'required|in:10,20,95',
-            'CustomerName' => 'required|string',
-            'CustomerContact' => 'nullable|string',
+            'customerName' => 'required|string',
+            'customerContact' => 'nullable|string',
             'billToAddress' => 'required|string',
             'billToCity' => 'required|string',
             'billToName' => 'required|string',
-            'billToStateId' => 'required|string',
             'billToZip' => 'required|string',
             'dateFirstShip' => 'required|date',
             'shipToAddress' => 'required|string',
             'shipToCity' => 'required|string',
-            'shipToCountryId' => 'required|string',
             'shipToName' => 'required|string',
-            'shipToStateId' => 'required|string',
             'shipToZip' => 'required|string',
             'taxRateName' => 'required|string',
-            // Added fields for customer creation
-            'addressName' => 'nullable|string',
-            'name' => 'nullable|string',
-            'city' => 'nullable|string',
-            'zip' => 'nullable|string',
-            'residentialFlag' => 'nullable|boolean',
-            'locationGroup' => 'nullable|string',
-            // Added other fields for sales order creation
+            //Added other fields for sales order creation
             'carrierName' => 'nullable|string',
             'carrierServiceName' => 'nullable|string',
             'billToCountryName' => 'nullable|string',
@@ -50,24 +41,28 @@ class SalesOrderController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
-        $data = $request->only([
-            'status', 'CustomerContact', 'billToAddress', 'billToCity', 'billToName', 'billToStateId', 'billToZip',
-            'dateFirstShip', 'shipToAddress', 'shipToCity', 'shipToCountryId', 'shipToName', 'shipToStateId',
-            'shipToZip', 'taxRateName', 'addressName', 'customerName', 'street', 'city', 'state', 'zip', 'isResidential',
-            'customerGroup', 'carrierName', 'carrierServiceName', 'billToCountryName', 'currencyName', 'fobPointName',
-            'locationGroupName', 'paymentTermsName', 'priorityName', 'qbClassName', 'registerName', 'salesmanName',
-            'shippingTermName'
-        ]);
+        // Generate Sales Order Number
+        $prefix = 10;
+        $lastOrder = SalesOrder::orderBy('id', 'desc')->first();
+        $lastNumber = $lastOrder ? intval(substr($lastOrder->num, strlen($prefix))) : 0;
+        $newNumber = $lastNumber + 1;
+        $orderNum = $prefix . str_pad($newNumber, 3, '0', STR_PAD_LEFT);
 
         // Find or create customer using Customer Controller
         $customerController = new CustomerController();
         $customer = $customerController->findOrCreateCustomer($request);
+        if ($customer instanceof \Illuminate\Http\JsonResponse) {
+            return $customer;
+        }
         $data['customerId'] = $customer->id;
 
-        /* still to be modified
+        /* still to be modified */
         $carrierController = new CarrierController();
         $data['carrierId'] = $carrierController->getCarrierId($request->carrierName);
         $data['carrierServiceId'] = $carrierController->getCarrierServiceId($request->carrierServiceName);
@@ -107,23 +102,77 @@ class SalesOrderController extends Controller
         $data['shipToStateId'] = $shippingData['shipToStateId'];
 
         $taxController = new TaxController();
-        $data['taxRateId'] = $taxController->getTaxRateId($request->taxRateName);
-        */
+        $taxData = $taxController->getTaxRateData($request->taxRateName);
+        $data['taxRateId'] = $taxData['taxRateId'];
+        $data['taxRateName'] = $taxData['taxRateName'];
 
-        // Find the highest SONum (num) and increment it
-        $prefix = 10;
-        $lastOrder = SalesOrder::orderBy('id', 'desc')->first();
-        $lastNumber = $lastOrder ? intval(substr($lastOrder->num, strlen($prefix))) : 0;
-        $newNumber = $lastNumber + 1;
-        $data['num'] = $prefix . str_pad($newNumber, 3, '0', STR_PAD_LEFT);
-
-        // Use today's date for dateCreate
-        $data['dateCreate'] = date('Y-m-d');
-
-        // Create the sales order with Num
-        $salesOrder = SalesOrder::create($data);
+        // Create the sales order
+        $salesOrder = SalesOrder::create([
+            'billToAddress' => $request->input('billToAddress'),
+            'billToCity' => $request->input('billToCity'),
+            'billToCountryId' => $request->input('billToCountryId'),
+            'billToName' => $request->input('billToName'),
+            'billToStateId' => $request->input('billToStateId'),
+            'billToZip' => $request->input('billToZip'),
+            'carrierId' => $data['carrierId'],
+            'carrierServiceId' => $data['carrierServiceId'],
+            'cost' => $request->input('cost'),
+            'currencyId' => $data['currencyId'],
+            'currencyRate' => $data['currencyRate'],
+            'customerContact' => $request->input('customerContact'),
+            'customerId' => $request->input('customerId'),
+            'customerPO' => $request->input('customerPO'),
+            'dateCompleted' => $request->input('dateCompleted'),
+            'dateCreated' => $request->input('dateCreated'),
+            'dateExpired' => $request->input('dateExpired'),
+            'dateFirstShip' => $request->input('dateFirstShip'),
+            'dateIssued' => $request->input('dateIssued'),
+            'dateLastModified' => $request->input('dateLastModified'),
+            'dateRevision' => $request->input('dateRevision'),
+            'email' => $request->input('email'),
+            'estimatedTax' => $request->input('estimatedTax'),
+            'fobPointId' => $data['fobPointId'],
+            'locationGroupId' => $data['locationGroupId'],
+            'mcTotalTax' => $request->input('mcTotalTax'),
+            'note' => $request->input('note'),
+            'num' => $orderNum,
+            'paymentTermsId' => $data['paymentTermsId'],
+            'phone' => $request->input('phone'),
+            'priorityId' => $data['priorityId'],
+            'qbClassId' => $data['qbClassId'],
+            'registerId' =>  $data['registerId'],
+            'residentialFlag' => $request->input('residentialFlag'),
+            'revisionNum' => $request->input('revisionNum'),
+            'salesman' => $data['salesman'],
+            'salesmanId' => $data['salesmanId'],
+            'salesmanInitials' => $data['salesmanInitials'],
+            'shipTermsId' => $data['shipTermsId'],
+            'shipToAddress' => $request->input('shipToAddress'),
+            'shipToCity' => $request->input('shipToCity'),
+            'shipToCountryId' => $data['shipToCountryId'],
+            'shipToName' => $request->input('shipToName'),
+            'shipToStateId' => $data['shipToStateId'],
+            'shipToZip' => $request->input('shipToZip'),
+            'statusId' => $request->input('statusId'),
+            'taxRate' => $request->input('taxRate'),
+            'taxRateId' => $data['taxRateId'],
+            'taxRateName' => $data['taxRateName'],
+            'toBeEmailedy' => $request->input('toBeEmailedy'),
+            'toBePrintedy' => $request->input('toBePrintedy'),
+            'totalIncludesTaxy' => $request->input('totalIncludesTaxy'),
+            'totalTax' => $request->input('totalTax'),
+            'subTotal' => $request->input('subTotal'),
+            'totalPrice' => $request->input('totalPrice'),
+            'typeId' => $request->input('typeId'),
+            'url' => $request->input('url'),
+            'username' => $request->input('username'),
+            'vendorPO' => $request->input('vendorPO'),
+        ]);
 
         return response()->json([
-            'message' => 'Sales Order created successfully', 'sales_order' => $salesOrder, 'all_fields' => $data]);
+            'message' => 'Sales Order created successfully',
+            'so' => $salesOrder
+        ]);
     }
 }
+
--- app/Models/SalesOrder.php
+++ app/Models/SalesOrder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,23 +10,17 @@ class SalesOrder extends Model
 
     protected $table = 'so';
 
-    // Specify the fillable fields
     protected $fillable = [
-        'status', 'customerName', 'customerContact', 'billToAddress',
-        'billToCity', 'billToCountryId', 'billToName', 'billToStateId', 'billToZip',
-        'carrierId', 'carrierServiceId', 'Cost', 'currencyId', 'currencyRate', 'customerId',
-        'customerPO', 'dateCompleted', 'dateCreate', 'dateExpired', 'dateFirstShip',
-        'dateIssued', 'dateLastModified', 'dateRevision', 'email', 'estimatedTax',
-        'fobPointId', 'locationGroupId', 'mcTotalTax', 'note', 'num', 'paymentTermsId',
-        'phone', 'priorityId', 'qbClassId', 'registerId', 'residentialFlag', 'revisionNum',
-        'salesman', 'salesmanId', 'salesmanInitials', 'shipTermsId', 'shipToAddress',
-        'shipToCity', 'shipToCountryId', 'shipToName', 'shipToStateId', 'shipToZip',
-        'statusId', 'taxRate', 'taxRateId', 'taxRateName', 'toBeEmailed', 'toBePrinted',
-        'totalIncludesTax', 'totalTax', 'subTotal', 'totalPrice', 'typeId', 'url',
+        'billToAddress', 'billToCity', 'billToCountryId', 'billToName', 'billToStateId', 'billToZip', 'carrierId',
+        'carrierServiceId', 'cost', 'currencyId', 'currencyRate', 'customerContact', 'customerId', 'customerPO', 'dateCompleted',
+        'dateCreated', 'dateExpired', 'dateFirstShip', 'dateIssued', 'dateLastModified', 'dateRevision', 'email', 'estimatedTax',
+        'fobPointId', 'locationGroupId', 'mcTotalTax', 'note', 'num', 'paymentTermsId', 'phone', 'priorityId', 'qbClassId',
+        'registerId', 'residentialFlag', 'revisionNum', 'salesman', 'salesmanId', 'salesmanInitials', 'shipTermsId', 'shipToAddress',
+        'shipToCity', 'shipToCountryId', 'shipToName', 'shipToStateId', 'shipToZip', 'statusId', 'taxRate', 'taxRateId',
+        'taxRateName', 'toBeEmailedy', 'toBePrintedy', 'totalIncludesTaxy', 'totalTax', 'subTotal', 'totalPrice', 'typeId', 'url',
         'username', 'vendorPO'
     ];
 
     // Disable timestamps
     public $timestamps = false;
-
 }
