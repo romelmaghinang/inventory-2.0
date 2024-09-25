c66735db7258f858c86a47ee6f5083408d6e89db
romelmaghinang <romel.maghinang@clairemontferrond.com>
2024-09-26 05:15:16 +0800

    sanctum and role permissions

--- routes/api.php
+++ routes/api.php
@@ -43,19 +43,17 @@ Route::middleware('auth:api')->group(function () {
                 'payment-terms' => PaymentTermsController::class,
             ]
         ); 
-    Route::post('pick-finish', FinishController::class);
-    Route::post('pick-start', StartController::class);
-    Route::post('pack', PackController::class);
-    Route::post('ship', ShipController::class);
-    Route::post('inventory', [InventoryController::class, 'store']);
-
-Route::middleware(['auth:sanctum', 'abilities:create-users,create-permission,create-role,assign-role,assign-permission'])->group(function () {
-    Route::post('/create-user', [UserController::class, 'createUser']);
-    Route::post('/create-role', [UserController::class, 'createRole']);
-    Route::post('/assign-role', [UserController::class, 'assignRole']);
-    Route::post('/assign-permission', [UserController::class, 'assignPermission']);
-    Route::post('/create-permission', [UserController::class, 'createPermission']);
-
-});
 
 
+        Route::middleware(['auth:sanctum', 'abilities:create-users,create-permission,create-role,assign-role,assign-permission,pick-finish,pick-start,pack,ship,inventory'])->group(function () {
+            Route::post('/create-role', [UserController::class, 'createRole']);
+            Route::post('/assign-role', [UserController::class, 'assignRole']);
+            Route::post('/assign-permission', [UserController::class, 'assignPermission']);
+            Route::post('/create-permission', [UserController::class, 'createPermission']);
+            Route::post('pick-finish', FinishController::class);
+            Route::post('pick-start', StartController::class);
+            Route::post('pack', PackController::class);
+            Route::post('ship', ShipController::class);
+            Route::post('inventory', [InventoryController::class, 'store']);
+        });
+        
