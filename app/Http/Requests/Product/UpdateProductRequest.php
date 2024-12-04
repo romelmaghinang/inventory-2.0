f6d142b010a851be68adef9b81f747fb405f1e3b
romelmaghinang <romel.maghinang@clairemontferrond.com>
2024-12-05 04:22:29 +0800

    update validation

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -25,35 +25,36 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'partNumber' => ['nullable', 'string', 'exists:part,num'], // partId
-            'productNumber' => ['nullable', 'string', 'max:70', 'unique:product,num,' . $this->route('product')], // num
-            'productDescription' => ['nullable', 'string', 'max:252'], // description
-            'productDetails' => ['required', 'string'], // details
-            'uom' => ['required', 'string', 'exists:uom,name'],
-            'price' => ['nullable', 'numeric'],
-            'class' => ['nullable', 'string'],
-            'active' => ['required', 'boolean'], // activeFlag
-            'taxable' => ['required', 'boolean'], // taxableFlag
-            'combo' => ['required', 'boolean'], // showSoComboFlag
-            'allowUom' => ['required', 'boolean'], // sellableInOtherUoms
-            'productUrl' => ['nullable', 'string', 'max:256'], // url
-            'productPictureUrl' => ['nullable', 'string', 'max:256'],
-            'productUpc' => ['nullable', 'string', 'max:41'], // upc
-            'productSku' => ['nullable', 'string', 'max:41'], // sku
-            'productSoItemType' => ['required', 'string', 'exists:soitemtype,name'], // defaultSoItemType
-            'incomeAccount' => ['required', 'string'],
-            'weight' => ['nullable', 'numeric'],
-            'weightUom' => ['nullable', 'string'], // weightUom 
-            'width' => ['nullable', 'numeric'],
-            'height' => ['nullable', 'numeric'],
-            'length' => ['nullable', 'numeric'],
-            'sizeUom' => ['nullable', 'string'],
-            'default' => ['required', 'boolean'],
-            'alertNote' => ['nullable', 'string', 'max:90'],
-            'cartonCount' => ['required', 'numeric'],
-            'cartonType' => ['required', 'string'],
-            'cf' => ['required', 'string'],
-        ];
+                'partNumber' => ['nullable', 'string', 'exists:part,num'], // partId
+                'productNumber' => ['nullable', 'string', 'max:70', 'unique:product,num,' . $this->route('product')], // num
+                'productDescription' => ['nullable', 'string', 'max:252'], // description
+                'productDetails' => ['nullable', 'string'], // details
+                'uom' => ['nullable', 'string', 'exists:uom,name'],
+                'price' => ['nullable', 'numeric'],
+                'class' => ['nullable', 'string'],
+                'active' => ['nullable', 'boolean'], // activeFlag
+                'taxable' => ['nullable', 'boolean'], // taxableFlag
+                'combo' => ['nullable', 'boolean'], // showSoComboFlag
+                'allowUom' => ['nullable', 'boolean'], // sellableInOtherUoms
+                'productUrl' => ['nullable', 'string', 'max:256'], // url
+                'productPictureUrl' => ['nullable', 'string', 'max:256'],
+                'productUpc' => ['nullable', 'string', 'max:41'], // upc
+                'productSku' => ['nullable', 'string', 'max:41'], // sku
+                'productSoItemType' => ['nullable', 'string', 'exists:soitemtype,name'], // defaultSoItemType
+                'incomeAccount' => ['nullable', 'string'],
+                'weight' => ['nullable', 'numeric'],
+                'weightUom' => ['nullable', 'string'], // weightUom 
+                'width' => ['nullable', 'numeric'],
+                'height' => ['nullable', 'numeric'],
+                'length' => ['nullable', 'numeric'],
+                'sizeUom' => ['nullable', 'string'],
+                'default' => ['nullable', 'boolean'],
+                'alertNote' => ['nullable', 'string', 'max:90'],
+                'cartonCount' => ['nullable', 'numeric'],
+                'cartonType' => ['nullable', 'string'],
+                'cf' => ['nullable', 'string'],
+            ];
+   
     }
 
     /**
@@ -64,13 +65,13 @@ class UpdateProductRequest extends FormRequest
     protected function failedValidation(Validator $validator)
     {
         $errors = $validator->errors();
-
+    
         $categorizedErrors = [
             'missingRequiredFields' => [],
             'invalidFormat' => [],
             'duplicateFields' => [],
         ];
-
+    
         foreach ($errors->messages() as $field => $messages) {
             foreach ($messages as $message) {
                 if (str_contains($message, 'required')) {
@@ -82,16 +83,20 @@ class UpdateProductRequest extends FormRequest
                 }
             }
         }
-
+    
+        if (empty(array_filter($categorizedErrors))) {
+            $categorizedErrors['validationErrors'] = $errors->messages();
+        }
+    
         $response = response()->json(
             [
                 'success' => false,
                 'message' => 'Validation errors occurred.',
-                'errors' => array_filter($categorizedErrors), 
+                'errors' => $categorizedErrors, 
             ],
             Response::HTTP_UNPROCESSABLE_ENTITY
         );
-
+    
         throw new HttpResponseException($response);
     }
 }
